b5af4c01a3aaad36b83822f709a2a60039c04835
Falon <falon4522@gmail.com>
2022-05-28 18:42:05 +0200

    final

--- ocr/main.py
+++ ocr/main.py
@@ -1,28 +1,36 @@
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+import jellyfish
+from scipy.ndimage import interpolation as inter
 
 
 class OCR:
-    def __init__(self, img_path, font='SegoeUI'):
-        self.letters = ['g', 'z', 'x', 'b', 'm', 'h', 'd', 'y', 'v', 'a', 's', 'p', 'q', 'o', 'e', 'c', 'f', 'n', 'j',
-                        'k', 'l',
-                        'w', 'r', 't', 'u', 'i']
-        self.numbers = []
-        self.special = []
+    def __init__(self, img_path, font='Sylfaen'):
+        self.patterns = ['1', '2', '8', '4', '5', '6', '7', '3', '9', '0', 'g', 'z', 'x', 'b', 'm', 'h', 'd', 'y', 'v',
+                         'a', 's', 'p', 'q', 'o', 'e', 'c', 'f', 'n', 'j',
+                         'k', 'l',
+                         'w', 'r', 't', 'u', 'i', 'comma', 'question', 'exclamation', 'dot']
+        self.special = ['comma', 'question', 'exclamation', 'dot']
+        self.special_map = {"comma": ",", "question": "?", "exclamation": "!", "dot": "."}
         self.scores = {}
         self.img = 255 - self.read_img(img_path)
-        # self.img = self.get_img_approx(self.img, 40)
         self.font_path = "Fonts/" + font + "/"
         self.pattern_imgs = self.get_pattern_imgs()
         self.pattern_positions = {}
 
     def run_ocr(self):
         self.get_max_score_for_each_pattern()
-        # self.show_img(self.img)
+        # self.img = self.get_img_approx(self.img, 40)
+        # self.img = self.img_denoise(self.img)
+        # self.img = self.correct_skew(self.img)
+        # self.img = self.deskew(self.img)
+        # self.rotate_img()
+        self.show_img(self.img)
         for p in self.pattern_imgs.keys():
-            self.single_pattern(p, 0.9, 1.1)
+            self.single_pattern(p, 0.93, 1.07)
         result = self.convert_to_text()
+        print(result)
 
         for pattern_key, positions in self.pattern_positions.items():
             print(f"{pattern_key} detected {len(positions)} times")
@@ -36,15 +44,9 @@ class OCR:
 
     def get_pattern_imgs(self):
         pattern_imgs = {}
-        # numbers = [str(i) for i in range(10)]
-        # special = ['dot', 'comma', 'exclamation', 'question']
 
-        for l in self.letters:
+        for l in self.patterns:
             pattern_imgs[l] = 255 - self.read_img(self.font_path + l + '.PNG')
-        for n in self.numbers:
-            pattern_imgs[n] = 255 - self.read_img(self.font_path + str(n) + '.PNG')
-        for s in self.special:
-            pattern_imgs[s] = 255 - self.read_img(self.font_path + s + '.PNG')
 
         return pattern_imgs
 
@@ -53,7 +55,6 @@ class OCR:
         y = np.fft.fft2(np.rot90(pattern, 2), x.shape)
         res = np.multiply(x, y)
         corr = np.abs(np.fft.ifft2(res)).astype(float)
-
         corr[corr < thresh * self.scores[key]] = 0
         corr[corr > upper_thresh * self.scores[key]] = 0
         corr[corr != 0] = 255
@@ -74,19 +75,11 @@ class OCR:
     def single_pattern(self, pattern_key, thresh, upper_thresh):
         pattern = self.pattern_imgs[pattern_key]
         pattern_corr = self.get_pattern_corr(pattern, thresh, upper_thresh, pattern_key)
-
         self.pattern_positions[pattern_key] = self.get_pattern_positions(pattern, pattern_corr)
-
-        # print(pattern_key)
-        # print(self.pattern_positions)
-        # self.show_img(pattern_corr)
-        # self.show_img(np.rot90(pattern_corr,2))
 
         for x, y in self.pattern_positions[pattern_key]:
             w, h = pattern.shape[:2]
             self.img[x - w: x, y - h: y] = 0
-
-        # self.show_img(self.img)
 
     def convert_to_text(self):
         positions = []
@@ -98,6 +91,7 @@ class OCR:
         line_height = 60
 
         lines = []
+
         lowest_y = positions_y[0][2]
         lines.append(([], lowest_y))
 
@@ -124,6 +118,8 @@ class OCR:
                         line_text += " "
                 else:
                     letter, x = line[0][i]
+                if letter in self.special:
+                    letter = self.special_map[letter]
                 line_text += letter
 
             result += line_text
@@ -137,26 +133,24 @@ class OCR:
 
     def get_max_score_for_each_pattern(self):
         scores = {}
-        for l in self.letters:
+        for l in self.patterns:
             pattern = self.pattern_imgs[l]
             x = np.fft.fft2(pattern)
             y = np.fft.fft2(np.rot90(pattern, 2), x.shape)
             res = np.multiply(x, y)
             corr = np.abs(np.fft.ifft2(res)).astype(float)
             scores[l] = np.max(corr)
+
         self.scores = scores
 
     def deskew(self, img):
         angle = self._getSkewAngle(img)
-        return self._rotateImage(img, -1.0 * angle)
+        return np.rot90(self._rotateImage(img, angle), 1)
 
     def _getSkewAngle(self, img):
         blur = cv2.GaussianBlur(img, (9, 9), 0)
         thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-        # Apply dilate to merge text into meaningful lines/paragraphs.
-        # Use larger kernel on X axis to merge characters into single line, cancelling out any spaces.
-        # But use smaller kernel on Y axis to separate between different blocks of text
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 5))
         dilate = cv2.dilate(thresh, kernel, iterations=5)
 
@@ -166,7 +160,6 @@ class OCR:
         largest_contour = contours[0]
         min_area_react = cv2.minAreaRect(largest_contour)
 
-        # Determine the angle. Convert it to the value that was originally used to obtain skewed image
         angle = min_area_react[-1]
         if angle < -45:
             angle = 90 + angle
@@ -189,41 +182,105 @@ class OCR:
         A = U.dot(S.dot(VT))
         return A
 
+    def img_denoise(self, img):
+        dst = cv2.fastNlMeansDenoising(img, None, 10, 7, 21)
+        return dst
 
-"""
-wnioski
-c zamazalo b  
-v zamazalo n i m
-c zamazalo n
-b zamzało g
-g poprawnie działa dla rotacji o 180 stopni
-n zamazało h
-c zamzało d
-v zamazało y
-c zamazało o i trochę p
-"""
+    def rotate_img(self):
+        img = cv2.bitwise_not(self.img)
+        thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        coords = np.column_stack(np.where(thresh > 0))
+        angle = cv2.minAreaRect(coords)[-1]
+        if angle < -45:
+            angle = -(90 + angle)
+        else:
+            angle = - angle
+
+        angle += 90
+
+        (h, w) = self.img.shape[:2]
+        center = (w // 2, h // 2)
+        M = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotated = cv2.warpAffine(self.img, M, (w, h),
+                                 flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+        plt.imshow(rotated)
+        plt.show()
+        self.img = rotated
+
+    def correct_skew(self, image, delta=10, limit=6):
+        def determine_score(arr, angle):
+            data = inter.rotate(arr, angle, reshape=False, order=0)
+            histogram = np.sum(data, axis=1, dtype=float)
+            score = np.sum((histogram[1:] - histogram[:-1]) ** 2, dtype=float)
+            return histogram, score
+
+        thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+        scores = []
+        angles = np.arange(-limit, limit + delta, delta)
+        for angle in angles:
+            histogram, score = determine_score(thresh, angle)
+            scores.append(score)
+
+        best_angle = angles[scores.index(max(scores))]
+
+        (h, w) = image.shape[:2]
+        center = (w // 2, h // 2)
+        M = cv2.getRotationMatrix2D(center, best_angle, 1.0)
+        corrected = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, \
+                                   borderMode=cv2.BORDER_REPLICATE)
+        return corrected
+
+
+def score(pred, true):
+    return jellyfish.jaro_distance(pred, true)
+
+
 if __name__ == '__main__':
-    mutilinerotated = 'Fonts/SegoeUI/mutilinerotated.PNG'
-    oneline_rotated = 'Fonts/SegoeUI/oneline_rotated.PNG'
-    oneline_rotated_corr = 'this is one line text'
-    special_chars = 'Fonts/SegoeUI/special_chars.PNG'
-    with_numbers = 'Fonts/SegoeUI/with_numbers.PNG'
-    test_file = 'Fonts/SegoeUI/test_file.PNG'
-    all_letters = 'Fonts/SegoeUI/all_letters.PNG'
-    all_letters_cutted = 'Fonts/SegoeUI/all_letters_cutted.PNG'
-    c_letter = 'Fonts/SegoeUI/c.PNG'
-    qwerty = 'Fonts/SegoeUI/qwertyuiop.PNG'
-    zxcvbnmasdfghjkl = 'Fonts/SegoeUI/zxcvbnmasdfghjkl.PNG'
-    all_oneline = 'Fonts/SegoeUI/all_oneline.PNG'
-    oneline_close = 'Fonts/SegoeUI/oneline_close.PNG'
+    # mutilinerotated = 'Fonts/SegoeUI/mutilinerotated.PNG'
+    # oneline_rotated = 'Fonts/SegoeUI/oneline_rotated.PNG'
+    # oneline_rotated_corr = 'this is one line text'
+    # special_chars = 'Fonts/SegoeUI/special_chars.PNG'
+    # with_numbers = 'Fonts/SegoeUI/with_numbers.PNG'
+    # test_file = 'Fonts/SegoeUI/test_file.PNG'
+    # all_letters = 'Fonts/SegoeUI/all_letters.PNG'
+    # all_letters_cutted = 'Fonts/SegoeUI/all_letters_cutted.PNG'
+    # c_letter = 'Fonts/SegoeUI/c.PNG'
+    # qwerty = 'Fonts/SegoeUI/qwertyuiop.PNG'
+    # zxcvbnmasdfghjkl = 'Fonts/SegoeUI/zxcvbnmasdfghjkl.PNG'
+    # all_oneline = 'Fonts/SegoeUI/all_oneline.PNG'
+    # oneline_close = 'Fonts/SegoeUI/oneline_close.PNG'
 
-    hello_mownit = 'Fonts/SegoeUI/hello_mownit.PNG'
+    # hello_mownit = 'Fonts/SegoeUI/hello_mownit.PNG'
     hello_mownit_ocr_test_multiline = 'Fonts/SegoeUI/hello_ocr_test_multiline.PNG'
+    hello_mownit_ocr_test_multiline_true = """hello mownit
+ocrtest
+multiline"""
 
-    proceeding_img = hello_mownit_ocr_test_multiline
+    test1 = 'Fonts/SegoeUI/test1.PNG'
+    test1_nr = 'Fonts/SegoeUI/test1_not_rotated.PNG'
+    test1_true = """mownit ocr text
+rotated 5 degress, 
+with commas and dots.
+the end ! is it?"""
 
-    ocr = OCR(proceeding_img)
-    ocr.get_max_score_for_each_pattern()
+    numbers = 'Fonts/SegoeUI/numbers.PNG'
+    numbers_true = """ocr 1 detect 2
+numbers 3 four
+4 5 6 7 8 9 0"""
+
+    special_test = 'Fonts/SegoeUI/special.PNG'
+    special_true = """hello, mownit!
+how are you?
+i am good."""
+
+    sylfaen_test1 = 'Fonts/Sylfaen/test1.PNG'
+    sylfaen_test2 = 'Fonts/Sylfaen/test2.PNG'
+    sylfaen_test3 = 'Fonts/Sylfaen/test3.PNG'
+    sylfaen_testnoisy = 'Fonts/Sylfaen/test1noisy.PNG'
+    sylfaen_testnoisier = 'Fonts/Sylfaen/test1noisier.PNG'
+    sylfaen_testnoisier2 = 'Fonts/Sylfaen/test1noise50.PNG'
+
     # deskew
     # img = ocr.read_img(proceeding_img)
     # ocr.show_img(img)
@@ -231,5 +288,12 @@ if __name__ == '__main__':
     # deskewd_img = ocr.deskew(img)
 
     # ocr.show_img(deskewd_img)
-    #######
-    ocr.run_ocr()
+    ##
+
+    proceeding_img = sylfaen_testnoisier
+
+    ocr = OCR(proceeding_img)
+    pred = ocr.run_ocr()
+
+    similarity = score(pred, hello_mownit_ocr_test_multiline_true)
+    print(similarity)
